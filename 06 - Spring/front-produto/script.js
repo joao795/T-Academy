53fd9ab90cd5c28fed53dde7091b16d520684c8f
João Marcelo Schneider da Silva e Souza <f0fp1417@t-systems.com.br>
2022-11-04 16:40:45 -0300

    2022-11-04

--- 06 - Spring/front-produto/script.js
+++ 06 - Spring/front-produto/script.js
@@ -1,7 +1,34 @@
-obterProdutos();
+let vetor = [];
+
+window.onload = function() {
+    obterProdutos();
+
+    listarProdutos();
+}
 
 function obterProdutos() {
     fetch("http://localhost:8080")
     .then(retorno => retorno.json())
-    .then(produtos => console.table(produtos))
+    .then(produtos => vetor = produtos)
+    .then(() => listarProdutos());
+}
+
+function listarProdutos() {
+    let tabela = document.getElementById("tabela");
+
+    tabela.innerHTML = "";
+
+    for (let i = 0; i < vetor.length; i++) {
+        let linha = tabela.insertRow(-1);
+
+        let colunaCodigo = linha.insertCell(0);
+        let colunaNome = linha.insertCell(1);
+        let colunaValor = linha.insertCell(2);
+        let colunaSelecionar = linha.insertCell(3);
+
+        colunaCodigo.innerText = vetor[i].codigo;
+        colunaNome.innerText = vetor[i].nome;
+        colunaValor.innerText = vetor[i].valor;
+        colunaSelecionar.innerHTML = "<button class='btn btn-success'>Selecionar</button>"
+    }
 }
