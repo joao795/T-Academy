f5e4dc15ef90cdbeab190dd20a28e0c1dbe62dda
João Marcelo Schneider da Silva e Souza <f0fp1417@t-systems.com.br>
2022-11-07 14:13:10 -0300

    2022-11-07

--- 06 - Spring/pessoa/src/main/java/br/com/projeto/pessoa/repositorio/PessoaRepositorio.java
+++ 06 - Spring/pessoa/src/main/java/br/com/projeto/pessoa/repositorio/PessoaRepositorio.java
@@ -1,5 +1,6 @@
 package br.com.projeto.pessoa.repositorio;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,32 @@ import br.com.projeto.pessoa.modelo.PessoaModelo;
 
 @Repository
 public interface PessoaRepositorio extends CrudRepository<PessoaModelo, Long> {
-    
+    //retornar todos os registros com uma determinada idade
+    Iterable<PessoaModelo> findByIdade(int idade);
+
+    //listar os registros através da coluna nome (A - Z)
+    Iterable<PessoaModelo> findByOrderByNome();
+
+    //listar os registros através da coluna nome (Z - A)
+    Iterable<PessoaModelo> findByOrderByNomeDesc();
+
+    //listar todas as pessoas com uma determinada idade em ordem decrescente de nome
+    Iterable<PessoaModelo> findByIdadeOrderByNomeDesc(int idade);
+
+    //listar pessoas cujos nomes possuem determinado termo
+    Iterable<PessoaModelo> findByNomeContaining(String termo);
+
+    //listar pessoas cujos nomes iniciam com determinado termo
+    Iterable<PessoaModelo> findByNomeStartsWith(String termo);
+
+    //listar pessoas cujos nomes finalizam com determinado termo
+    Iterable<PessoaModelo> findByNomeEndsWith(String termo);
+
+    //método para retornar a soma das idades (JPQL)
+    @Query(value = "select sum(idade) from pessoas", nativeQuery = true)
+    int somaIdades();
+
+    //método para retornar pessoas com idade superior a 20 anos
+    @Query(value = "select * from pessoas where idade >= ?", nativeQuery = true)
+    Iterable<PessoaModelo> idadeSuperior(int idade);
 }
